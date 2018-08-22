21bed0eba1df900377af82be173df0670a681f35
Nick Meldrum <nick@nickmeldrum.com>
2018-08-22 13:12:03 +0100

    get both endpoints into reactsite

--- react-site/src/index.js
+++ react-site/src/index.js
@@ -9,8 +9,13 @@ import InvoiceScreen from './components/InvoiceScreen';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const graphQlEndpoint = {
+  local: 'http://localhost:3000/graphql',
+  staging: 'https://w5ao7vl6hreord7x7ledo7cq5i.appsync-api.us-east-1.amazonaws.com/graphql',
+  prod: 'tbd',
+}
 const appsyncClient = new AWSAppSyncClient({
-  url: 'http://localhost:3000/graphql',
+  url: graphQlEndpoint.local,
   region: 'us-east-1',
   auth: { type: 'API_KEY', apiKey: 'da2-w5isxtvkdjdhxmb7pfucndiypy' }
 });
