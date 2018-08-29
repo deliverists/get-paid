663c01bcaf9a63f8b71fc83b9e01a6c8f21f60a4
Nick Meldrum <nick@nickmeldrum.com>
2018-08-29 08:35:28 +0100

    remove old clients folders

--- mobile/lib/AppSync.js
+++
@@ -1,7 +0,0 @@
-﻿export default {
-    "graphqlEndpoint": "https://w5ao7vl6hreord7x7ledo7cq5i.appsync-api.us-east-1.amazonaws.com/graphql",
-    // "graphqlEndpoint": "http://localhost:3000/graphql",
-    "region": "us-east-1",
-    "authenticationType": "API_KEY",
-    "apiKey": "da2-w5isxtvkdjdhxmb7pfucndiypy",
-}
